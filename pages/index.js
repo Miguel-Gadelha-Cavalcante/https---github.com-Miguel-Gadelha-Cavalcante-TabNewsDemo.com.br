08bc00d34c303192120a84b5ddeda0f92734e713
Miguel-Gadelha-Cavalcante <mgadelhac4@gmail.com>
2024-06-19 17:55:03 -0300

    Third commit

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home () {
+    return <h1>Desculpa Sergio, caguei o pau por algo simples, pelo menos voltei ao mesmo lugar que eutava em pouco tempo, vamos indo e continuando</h1>
+}
+
+export default Home;
