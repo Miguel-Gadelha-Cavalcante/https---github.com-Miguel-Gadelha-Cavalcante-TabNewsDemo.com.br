28cf5b238e3a8f86723277264389e4694524900e
Miguel-Gadelha-Cavalcante <mgadelhac4@gmail.com>
2024-07-11 17:27:46 -0300

    aspirador

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <img class="foto" src="https://i.redd.it/ue35q8snx73z.jpg" alt="Yorick and his incredible argument"></img>//<h1>Continue em frente😁👍</h1>
+    return <h1>Aspirador, aspira a dor, a dor ele aspira, aspirada é a dor.</h1>
 }
 
 export default Home;
