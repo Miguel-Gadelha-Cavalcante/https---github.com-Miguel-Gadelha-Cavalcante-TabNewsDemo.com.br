e8c624a56759833a8dcfd6f0878d35c1c67ce8c5
Miguel-Gadelha-Cavalcante <mgadelhac4@gmail.com>
2024-07-11 17:23:52 -0300

    mudanças de planos

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Continue em frente😁👍</h1>
+    return <img class="foto" src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.ebay.com%2Fitm%2F165920543651&psig=AOvVaw1tOaY1C4Yxd0NsEBM9jdMd&ust=1720815802508000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCPiB_Mbon4cDFQAAAAAdAAAAABAE" alt="Yorick and his incredible argument"></img>//<h1>Continue em frente😁👍</h1>
 }
 
 export default Home;
