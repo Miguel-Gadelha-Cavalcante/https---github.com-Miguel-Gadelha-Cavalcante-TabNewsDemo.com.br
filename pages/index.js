84ea73756c6a521e34f513215fabbad0f318e534
Miguel-Gadelha-Cavalcante <mgadelhac4@gmail.com>
2024-07-11 17:30:25 -0300

    prendi galera

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home () {
-    return <h1>Aspirador, aspira a dor, a dor ele aspira, aspirada é a dor.</h1>
+    return (<img class="foto" src="https://imgb.ifunny.co/images/cc11253211fb14f5545177f04e176c14e3208ade2697a7105bf1837673b5a55f_1.webp" alt="Yorick and his incredible argument"></img>
+        
+    )
 }
 
 export default Home;
