24e9e20e618168efa9fb73bc52b5b0b64d8d95dd
Miguel-Gadelha-Cavalcante <mgadelhac4@gmail.com>
2024-11-08 17:31:07 -0300

    é isso

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 function Home() {
-    return (
-        <div>
-            <img class="foto" src="https://imgb.ifunny.co/images/cc11253211fb14f5545177f04e176c14e3208ade2697a7105bf1837673b5a55f_1.webp" alt="Yorick and his incredible argument"></img>,
-            <h1>Mais um Dia, me deseje sorte.</h1>
-        </div>
-    );
-
+  return (
+    <div>
+      <img
+        class="foto"
+        src="https://imgb.ifunny.co/images/cc11253211fb14f5545177f04e176c14e3208ade2697a7105bf1837673b5a55f_1.webp"
+        alt="Yorick and his incredible argument"
+      ></img>
+      ,<h1>Mais um Dia, me deseje sorte.</h1>
+    </div>
+  );
 }
 
 export default Home;
