a8bdfdbe9b2d531f9f4502bbdf83742f01717b93
Miguel-Gadelha-Cavalcante <mgadelhac4@gmail.com>
2024-11-08 17:24:43 -0300

    e

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
-function Home () {
-    return <h1>Vamos indo que to devendo até pra deus</h1>//<img class="foto" src="https://imgb.ifunny.co/images/cc11253211fb14f5545177f04e176c14e3208ade2697a7105bf1837673b5a55f_1.webp" alt="Yorick and his incredible argument"></img>
-        
-    
+function Home() {
+    return (
+        <div>
+            <img class="foto" src="https://imgb.ifunny.co/images/cc11253211fb14f5545177f04e176c14e3208ade2697a7105bf1837673b5a55f_1.webp" alt="Yorick and his incredible argument"></img>,
+            <h1>Mais um Dia, me deseje sorte.</h1>
+        </div>
+    );
+
 }
 
 export default Home;
